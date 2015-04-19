6c4a1da7f36c2b9426c35600b5660358ce797b7d
Norbert Harder <n.harder@d3vnull.de>
2015-04-19 15:13:05 +0200

    Added scaling so that raytracing works, Popover on click on Asteroid, Cleanup

--- js/model.js
+++ js/model.js
@@ -1,55 +1,245 @@
-var model = null;
+var ASCAVIS = {};
 
-var camera, scene;
-var geometry, material, mesh;
-
-function main() {
-	jQuery.getJSON( "http://127.0.0.1:5000/model", function(data) {
-	 	initializeModel(data); 
-	 } )
+ASCAVIS.Model = (function(){
+	var AU = 1.496e8;
 	
-	var timer = $.timer(function() {
-	    updateModel(new Date());
-	    drawModel();
-	    lighting();
-	});
-	timer.set({ time : 1000, autostart : true });
-}
-
-function initializeModel(data) {
-	model = data;
-}
-
-function updateModel(date) {
-	if(model == null) return;
-	 
-	$.each(model, function(index,value) {
-		var request = new Object();
-		request.line1 = value.tle[0].line1;
-		request.line2 = value.tle[0].line2;
-		request.year = date.getFullYear();
-		request.month = date.getMonth()+1;
-		request.day = date.getDate();
-		request.hour = date.getHours();
-		request.minute = date.getMinutes();
-		request.second = date.getSeconds();
+	/** A THREE.js curve describing an orbit */
+	OrbitalCurve = function ( orbitalElements ) {
+	    // console.log("run constructor");
+	    this.orbitalElements = orbitalElements;
+	};
+	
+	OrbitalCurve.prototype = Object.create( THREE.Curve.prototype );
+	
+	OrbitalCurve.prototype.constructor = THREE.OrbitalCurve;
+	
+	OrbitalCurve.prototype.getPoint = function ( t ) {
+	    var coord = new calculateCartesianCoordinates(this.orbitalElements, t * 2 * Math.PI);
+	    // console.log(t, coord);
+	    return coord;
+	};
+	
+	function orbitalElements() {
+		this.referenceDate = new Date(2000, 1, 1, 1, 1, 1, 1);
+		this.orbitalPeriod = 1;
+		this.semiMajorAxis = 1;
+		this.eccentricity = 0;
+		this.meanAnomaly = 0;
+		this.ascendingNode = 0;
+		this.perihelion = 0;
+		this.inclination = 0;
+	}
+	
+	function cartesianCoordinates() {
+		this.x = 0;
+		this.y = 0;
+		this.z = 0;
+	}
+	
+	function trueAnomaly(orbitalElements, time) {
+		var timeSpan = (time.getTime() - orbitalElements.referenceDate.getTime()) / (60 * 60 * 24 * 1000 * 365.25);
+	    // FIXME: the following is actually the mean anomaly, a transformation is missing
+	    return orbitalElements.meanAnomaly + timeSpan / orbitalElements.orbitalPeriod * 2.0 * Math.PI
+	}
+	
+	function calculateCartesianCoordinates(orbitalElements, trueAnomaly) {
+	
+	    // Transformation matrix
+	    var cos_Om = Math.cos(orbitalElements.ascendingNode);
+	    var sin_Om = Math.sin(orbitalElements.ascendingNode);
+	    var cos_w = Math.cos(orbitalElements.perihelion);
+	    var sin_w = Math.sin(orbitalElements.perihelion);
+	    var cos_i = Math.cos(orbitalElements.inclination);
+	    var sin_i = Math.sin(orbitalElements.inclination);
+	
+		var x1 = cos_Om * cos_w - sin_Om * cos_i * sin_w;
+		var x2 = sin_Om * cos_w + cos_Om * cos_i * sin_w;
+		var x3 = sin_i * sin_w;
 		
-		$.ajax({
-		    type: 'POST',
-		    // Provide correct Content-Type, so that Flask will know how to process it.
-		    contentType: 'application/json',
-		    // Encode your data as JSON.
-		    data: JSON.stringify(request),
-		    // This is the type of data you're expecting back from the server.
-		    dataType: 'json',
-		    url: 'http://127.0.0.1:5000/calculate',
-		    success: function (a) {
-		    	var response = a;
-		        value.position = response.position;
-		        value.velocity = response.velocity;
-		        value.updated = true;
-		    }
-		});
-	} );
-}
-
+		var y1 = - cos_Om * sin_w - sin_Om * cos_i * cos_w;
+		var y2 = - sin_Om * sin_w + cos_Om * cos_i * cos_w;
+		var y3 = sin_i * cos_w;
+		
+		var z1 = sin_i * sin_Om;
+		var z2 = - sin_i * cos_Om;
+		var z3 = cos_i;
+	
+	    // Orbital state vector w.r.t. orbital plane
+		var r_v = orbitalElements.semiMajorAxis
+	        * (1.0 - Math.pow(orbitalElements.eccentricity, 2.0))
+	        / (1.0 + orbitalElements.eccentricity * Math.cos(trueAnomaly)); 
+		var v1 = r_v * Math.cos(trueAnomaly);
+		var v2 = r_v * Math.sin(trueAnomaly);
+	    //return new THREE.Vector3(v1, v2, 0);
+	
+	    // Transform to global coordinate system
+	    return new THREE.Vector3(
+	        v1 * x1 + v2 * y1,
+		    v1 * x2 + v2 * y2,
+		    v1 * x3 + v2 * y3
+	    );
+	}
+	
+	
+	Date.prototype.setJulian = function(jd) {
+	    var refDate = 2440587.5;
+	    var timestamp = (jd - refDate) * 86400000.0;
+	    this.setTime(timestamp);
+	}
+	
+	var Planets = (function() {
+		function planet(name, radius, color, orbitalElements) {
+			this.name = name;
+			this.radius = radius;
+			this.color = color;
+			this.orbitalElements = orbitalElements;
+		};	
+		
+		var earthOrbitalElements = new orbitalElements();
+		earthOrbitalElements.semiMajorAxis = 1 * AU;
+		earthOrbitalElements.orbitalPeriod = 1;
+		earthOrbitalElements.eccentricity = 0.0167;
+		earthOrbitalElements.referenceDate = new Date("January 1, 2000 12:00:00 UTC");
+		earthOrbitalElements.meanAnomaly = 355.53 / 180.0 * Math.PI;
+		earthOrbitalElements.ascendingNode = -11.26064 / 180.0 * Math.PI;
+		earthOrbitalElements.perihelion =  102.94719 / 180.0 * Math.PI;
+		earthOrbitalElements.inclination = 0.00005 / 180.0 * Math.PI;
+		 
+		var earth = new planet('earth', 6378, 0x1fd8ff, earthOrbitalElements);
+		
+		
+		var marsOrbitalElements = new orbitalElements();
+		marsOrbitalElements.semiMajorAxis = 1.666 * AU;
+		marsOrbitalElements.orbitalPeriod = 1.8808;
+		marsOrbitalElements.eccentricity = 0.0935;
+		marsOrbitalElements.referenceDate = new Date("January 1, 2000 12:00:00 UTC");
+		marsOrbitalElements.meanAnomaly = 19.3564 / 180.0 * Math.PI;
+		marsOrbitalElements.ascendingNode = 49.562 / 180.0 * Math.PI;
+		marsOrbitalElements.perihelion =  286.537 / 180.0 * Math.PI;
+		marsOrbitalElements.inclination = 5.65 / 180.0 * Math.PI;
+		 
+		var mars = new planet('mars', 3396, 0xff8040, marsOrbitalElements);
+		
+		return {
+			getPlanets : function(){
+				return [earth, mars];
+			}
+		}
+	}());
+	
+	var Asteroids = (function() {
+		var asteroids = [];
+		var jqxhr;
+		
+		function asteroid(id, orbitalElements) {
+			this.id = id;
+			this.orbitalElements = orbitalElements;
+			this.name = "";
+			this.taxonomyClass = "M";
+			this.orbitType = 0;
+			this.absoluteMagnitude = 0;
+			this.radius = 1;
+		};
+		
+		function selectionProperties() {
+			this.taxonomyClass = "all";
+			this.orbitType = -1;
+			this.minAbsoluteMagnitude = -5;
+			this.maxAbsoluteMagnitude = 10;
+			this.minDiameter = 100;
+			this.maxDiameter = 500;		
+		}
+		
+		var highlightProperties = new selectionProperties();
+		var filterProperties = new selectionProperties();
+		
+			
+		function loadAsteroidData(){
+				var selectionString = "/mpc_more/?orderby=absolute_magnitude%20DESC&no=1000&paramlim="
+				var taxonomyClassStringPart = "";
+				var orbitTypeStringPart = "";
+				
+				if(!(filterProperties.taxonomyClass == "all" || filterProperties.taxonomyClass == null || filterProperties.taxonomyClass == "")){
+					taxonomyClassStringPart = "taxonomy_class=%22"+ filterProperties.taxonomyClass +"%22%20AND%20";
+				}
+							
+				if(filterProperties.orbitType != -1){
+					orbitTypeStringPart = "orbit_type="+ filterProperties.orbitType +"%20AND%20"
+				}
+				
+				selectionString += taxonomyClassStringPart + orbitTypeStringPart + "absolute_magnitude>"+ filterProperties.minAbsoluteMagnitude +"%20AND%20absolute_magnitude<" + filterProperties.maxAbsoluteMagnitude + "%20AND%20diameter>"+filterProperties.minDiameter+"%20AND%20diameter<" + filterProperties.maxDiameter; 
+				
+				if(jqxhr && jqxhr.readyState != 4){
+				    jqxhr.abort();
+				}
+				jqxhr = $.getJSON( selectionString, function() {
+				  // console.log( "success" );
+				})
+				  .done(function(data) {
+				  		asteroids = [];		  		
+				  		data.forEach( function(a){
+				  			var asteroidOrbitalElements = new orbitalElements();
+				  						  			
+				  			asteroidOrbitalElements.referenceDate.setJulian(a.epoch_jd);
+				  			asteroidOrbitalElements.orbitalPeriod = a.period;
+				  			asteroidOrbitalElements.semiMajorAxis = a.semimajor_axis * AU;
+				  			asteroidOrbitalElements.eccentricity = a.eccentricity;
+				  			asteroidOrbitalElements.meanAnomaly = a.mean_anomaly / 180.0 * Math.PI;
+				  			asteroidOrbitalElements.ascendingNode = a.ascending_node / 180.0 * Math.PI;
+				  			asteroidOrbitalElements.perihelion = a.argument_of_perihelion / 180.0 * Math.PI;
+				  			asteroidOrbitalElements.inclination = a.inclination / 180.0 * Math.PI;
+				  			
+				  			var ast = new asteroid(a.number, asteroidOrbitalElements);
+				  			ast.name = a.name;
+				  			ast.radius = a.diameter / 2;
+				  			
+				  			asteroids.push(ast);
+				  		});
+				  })
+				  .fail(function() {
+				    console.log( "Asteroid AJAX request failed or canceled." );
+				  })
+				  .always(function() {
+				    //console.log( "complete" );
+				  });
+				  
+				jqxhr.complete(function() {
+				  //console.log( "second complete" );
+				});
+			};
+		
+		return {
+			load: function(){
+				loadAsteroidData();
+			},
+			getAsteroids: function(){
+				return asteroids;
+			},
+			getFilterProperties: function(){
+				return filterProperties;
+			},
+			getHighlightProperties: function(){
+				return highlightProperties;
+			},
+			setFilterProperties: function(selectionProperties){
+				filterProperties = selectionProperties;
+				loadAsteroidData();
+			},
+			setHighlightProperties: function(selectionProperties){
+				highlightProperties = selectionProperties;
+			}		
+		}	
+	}());
+	
+	return {
+		Asteroids: Asteroids,
+		Planets: Planets,
+		AU : AU,
+		calculateCartesianCoordinates: function(orbitalElements, trueAnomaly) {
+			return calculateCartesianCoordinates(orbitalElements, trueAnomaly);
+		},
+		trueAnomaly: function(orbitalElements, time){
+			return trueAnomaly(orbitalElements, time);
+		}		
+	}	
+}());
---
+++ js/renderer.js
@@ -0,0 +1,311 @@
+ASCAVIS.Renderer = (function(){
+	var renderer;
+	var camera;
+	var scene;
+	
+	var drawArray = new Array();
+	
+	var asteroidScaleFactor = 50;
+	var planetScaleFactor = 0.03;
+	var sunScaleFactor = 0.0005;
+	var distanceScaleFactor = 1e-5;
+	
+	var planetRenderModels = [];
+	var planetRenderModel = function(planet){
+	    // NOTE: radius is scaled up extremely for debugging purposes
+		var geometry = new THREE.SphereGeometry( planet.radius * planetScaleFactor, 16, 16 );
+		var material  = new THREE.MeshBasicMaterial( { color: planet.color, overdraw : true })
+		material.specular  = new THREE.Color(planet.color);
+		var mesh = new THREE.Mesh( geometry, material );	
+	
+	    var curve = new OrbitalCurve(planet.orbitalElements);
+		var orbitGeometry = new THREE.Geometry();
+		orbitGeometry.vertices = curve.getPoints(50);
+		
+		orbitGeometry.vertices.forEach(function(vertice){
+			vertice.x = vertice.x * distanceScaleFactor;
+			vertice.y = vertice.y * distanceScaleFactor;
+			vertice.z = vertice.z * distanceScaleFactor;
+		});
+		
+		var material = new THREE.LineBasicMaterial({ color : planet.color });
+		var orbit = new THREE.Line(orbitGeometry, material);
+		
+		this.planet = planet;
+		this.geometry = geometry;
+		this.material = material;
+		this.mesh = mesh;
+		this.orbitMesh = orbit;
+	}
+	
+	var asteroidRenderMap = {};
+	var asteroidRenderModel = function(asteroid, radius, color){
+		    // NOTE: radius is scaled up extremely for debugging purposes
+			var geometry = new THREE.SphereGeometry( radius * asteroidScaleFactor, 16, 16 );
+			//var geometry = new THREE.CubeGeometry( radius, radius, radius );
+			var material  = new THREE.MeshBasicMaterial( { color: color, overdraw : true })
+			material.specular  = new THREE.Color(0xffffff);
+			mesh = new THREE.Mesh( geometry, material );	
+		
+		    var curve = new OrbitalCurve(asteroid.orbitalElements);
+			var orbitGeometry = new THREE.Geometry();
+			
+			orbitGeometry.vertices = curve.getPoints(50);
+			orbitGeometry.vertices.forEach(function(vertice){
+				vertice.x = vertice.x * distanceScaleFactor;
+				vertice.y = vertice.y * distanceScaleFactor;
+				vertice.z = vertice.z * distanceScaleFactor;
+			});
+						
+			var material = new THREE.LineBasicMaterial({ color : 0xffffff });
+			var orbit = new THREE.Line(orbitGeometry, material);
+			
+			this.asteroid = asteroid;
+			this.geometry = geometry;		
+			this.material = material;
+			this.mesh = mesh;
+			this.orbitMesh = orbit;
+			
+			this.updateMarker = false;
+	}
+	
+	
+	function setUpScene() {
+		var container = document.getElementById("main");					
+		
+		renderer = new THREE.WebGLRenderer({ alpha: true });
+		renderer.setSize( container.offsetWidth, container.offsetHeight );
+		container.appendChild(renderer.domElement);
+			
+		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 2000 * ASCAVIS.Model.AU );
+		camera.position.set(0, -2 * ASCAVIS.Model.AU * distanceScaleFactor, 2 * ASCAVIS.Model.AU * distanceScaleFactor);
+		
+		camera.lookAt(new THREE.Vector3(0, 0, 0));
+	
+		controls = new THREE.OrbitControls(camera,container);
+		controls.addEventListener( 'change', render );
+		
+		scene = new THREE.Scene();
+		
+		setUpSun();
+		
+		ASCAVIS.Model.Planets.getPlanets().forEach( function(planet){		
+			pR = new planetRenderModel(planet);
+			planetRenderModels.push(pR);
+			
+			scene.add(pR.mesh);
+			scene.add(pR.orbitMesh);
+		});
+		
+		render();
+	
+		ASCAVIS.Model.Asteroids.load();
+	}
+	
+	function setUpSun(){
+		var geometry = new THREE.SphereGeometry( 695800 * sunScaleFactor , 16, 16 );
+		var material  = new THREE.MeshBasicMaterial( { color: 0xffffff });
+		
+		material.specular  = new THREE.Color('yellow');
+		mesh = new THREE.Mesh( geometry, material );
+		mesh.position.set(0,0,0);
+		scene.add(mesh);
+	}
+	
+	function drawAxes() {
+		var length = 2 * ASCAVIS.Model.AU;
+		var axes = new THREE.Object3D();
+		axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( length, 0, 0 ), 0xFF0000, false ) );
+		axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, length, 0 ), 0xFF0000, false ) );
+		axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, length ), 0xFF0000, false ) );
+		scene.add(axes);
+	}
+	
+	function updateScene(){
+		var currentdate = new Date();
+		
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		
+		
+		planetRenderModels.forEach( function(pR){
+	        var nu = ASCAVIS.Model.trueAnomaly(pR.planet.orbitalElements, currentdate);
+			var coordinates = ASCAVIS.Model.calculateCartesianCoordinates(pR.planet.orbitalElements, nu);	
+			pR.mesh.position.set(coordinates.x * distanceScaleFactor, coordinates.y * distanceScaleFactor, coordinates.z * distanceScaleFactor);
+		});
+
+		for (var key in asteroidRenderMap) {
+		    asteroidRenderMap[key].updateMarker = false;
+		}
+
+		asteroids = ASCAVIS.Model.Asteroids.getAsteroids();	
+		if(asteroids.length > 20){
+			asteroids.forEach(
+				function(ast){
+					var nu = ASCAVIS.Model.trueAnomaly(ast.orbitalElements, currentdate);
+					var coordinates = ASCAVIS.Model.calculateCartesianCoordinates(ast.orbitalElements, nu);	
+					
+					if(ast.id in asteroidRenderMap){
+						var rA = asteroidRenderMap[ast.id];
+						rA.mesh.position.set(coordinates.x * distanceScaleFactor, coordinates.y * distanceScaleFactor, coordinates.z * distanceScaleFactor);
+						rA.updateMarker = true;
+					}
+					else{
+						var rA = new asteroidRenderModel(ast, 1, 0xffffff);
+						rA.mesh.position.set(coordinates.x * distanceScaleFactor, coordinates.y * distanceScaleFactor, coordinates.z * distanceScaleFactor);
+						rA.updateMarker = true;
+						asteroidRenderMap[ast.id] = rA;
+						scene.add(rA.mesh);
+						scene.add(rA.orbitMesh);
+					}
+				}
+			);
+		}
+		else{					
+			asteroids.forEach(
+				function(ast){
+					var nu = ASCAVIS.Model.trueAnomaly(ast.orbitalElements, currentdate);
+					var coordinates = ASCAVIS.Model.calculateCartesianCoordinates(ast.orbitalElements, nu);	
+										
+					if(ast.id in asteroidRenderMap){
+						scene.remove(asteroidRenderMap[ast.id].mesh);
+						scene.remove(asteroidRenderMap[ast.id].orbitMesh);							
+						delete asteroidRenderMap[ast.id];
+					}
+
+					var rA = new asteroidRenderModel(ast, 1, 0x1FD8FF);
+					rA.mesh.position.set(coordinates.x * distanceScaleFactor, coordinates.y * distanceScaleFactor, coordinates.z * distanceScaleFactor);
+					rA.updateMarker = true;
+					asteroidRenderMap[ast.id] = rA;
+					scene.add(rA.mesh);
+					scene.add(rA.orbitMesh);
+				}
+			);
+		}
+		
+		for (var key in asteroidRenderMap) {
+		    if(asteroidRenderMap[key].updateMarker == false){
+		    	scene.remove(asteroidRenderMap[key].mesh);
+		    	scene.remove(asteroidRenderMap[key].orbitMesh);
+		    	delete asteroidRenderMap[key];
+		    }
+		}
+	}
+	
+	function drawLabel(text, position3d) {
+		var div = document.createElement('div');
+		div.className = 'annotation';
+		div.style.position = 'absolute';
+		//text2.style.zIndex = 1;    // if you still don't see the label, try uncommenting this
+		div.innerHTML = text;
+		div.style.top = Math.max(Math.min(world3dToCanvas2d(position3d).y,window.innerHeight - 100), 100 ) + 'px';
+		div.style.left = Math.max(Math.min(world3dToCanvas2d(position3d).x,window.innerWidth - 100), 100) + 'px';
+		
+		div.onclick = function(){
+			moveCameraTo(position3d);
+		};
+		
+		document.body.appendChild(div);
+		return div;
+	}
+	
+	function updateLabel( annotation, position3d) {
+		if(world3dToCanvas2d(position3d).y > window.innerHeight - 50 || world3dToCanvas2d(position3d).y < 50){ $(annotation).hide(); return; }
+		if(world3dToCanvas2d(position3d).x > window.innerWidth - 50 || world3dToCanvas2d(position3d).x < 50) { $(annotation).hide(); return; }
+	
+		annotation.style.top = world3dToCanvas2d(position3d).y;
+		annotation.style.left = world3dToCanvas2d(position3d).x;
+	
+		$(annotation).show();
+	
+		annotation.onclick = function(){
+			moveCameraTo(position3d);
+		};
+	
+		return annotation;
+	}
+	
+	function world3dToCanvas2d( position ) {
+			var projector = new THREE.Projector();
+			var vector = projector.projectVector(position.clone(), camera);
+			vector.x = (vector.x + 1)/2 * window.innerWidth;
+			vector.y = -(vector.y - 1)/2 * window.innerHeight;
+			return vector;
+	}
+	
+	function moveCameraTo(position){
+		var tween = new TWEEN.Tween(camera.position).to({
+		    x: position.x,
+		    y: position.y,
+		    z: 1
+		}).easing(TWEEN.Easing.Linear.None).onUpdate(function () {
+		    camera.lookAt(camera.target);
+		}).onComplete(function () {
+		    camera.lookAt(position);
+		}).start();
+		
+		var tween = new TWEEN.Tween(camera.target).to({
+		    x: position.x,
+		    y: position.y,
+		    z: 0
+		}).easing(TWEEN.Easing.Linear.None).onUpdate(function () {
+		}).onComplete(function () {
+		    camera.lookAt(position);
+		}).start();
+	}
+	
+	function lighting() {
+		// add subtle blue ambient lighting
+		var ambientLight = new THREE.AmbientLight(0x000044);
+		scene.add(ambientLight);
+		
+		// directional lighting
+		var directionalLight = new THREE.DirectionalLight(0xffffff);
+		directionalLight.position.set(1, 1, 1).normalize();
+		scene.add(directionalLight);
+	}
+	
+	function objectFrom2DCoordinates(x, y){
+		raycaster = new THREE.Raycaster();
+		raycaster.setFromCamera( new THREE.Vector2(x,y), camera );
+		intersects = raycaster.intersectObjects( scene.children );
+		
+		var intersects = raycaster.intersectObjects( scene.children );
+		
+		if ( intersects.length > 0 ) {
+			for (var key in asteroidRenderMap) {
+			    if(intersects[0].object === asteroidRenderMap[key].mesh){
+			    	return asteroidRenderMap[key];
+			    }
+			}
+		}
+		
+		return null;
+	}	
+	
+	function render() {
+		renderer.render(scene, camera);
+	}
+	
+	return {
+		setUpScene : function(){
+			setUpScene();
+		},
+		updateScene : function(){
+			updateScene();
+		},
+		render : function(){
+			render();
+		},
+		objectFrom2DCoordinates : function(){
+			return objectFrom2DCoordinates(x, y);
+		},
+		getCanvasWidth: function(){
+			return renderer.domElement.width;
+		},
+		getCanvasHeight : function(){
+			return renderer.domElement.height;
+		}
+	}
+}());
